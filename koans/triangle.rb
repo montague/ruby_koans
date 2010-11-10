ae6a562711227121fbff9e73db9d0afdb0b39f83
ian <ian.asaff@gmail.com>
2010-11-09 19:25:46 -0500

    11.9.2010 pm

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -13,8 +13,22 @@
 # and
 #   about_triangle_project_2.rb
 #
-def triangle(a, b, c)
-  # WRITE THIS CODE
+def triangle(a,b,c)
+  raise TriangleError unless is_valid_triangle?(a,b,c)
+  h = [a]
+  h << b unless h.include?(b)
+  h << c unless h.include?(c)
+  return :equilateral if h.length == 1
+  return :isosceles if h.length == 2
+  :scalene
+end
+
+def is_valid_triangle?(*a)
+  if ((a[0] + a[1]) <= a[2]) || ((a[1]+a[2]) <= a[0]) || ((a[0]+a[2]) <= a[1])
+    return false
+  end
+  a.each {|i| return false if i <= 0} 
+  true
 end
 
 # Error class used in part 2.  No need to change this code.
